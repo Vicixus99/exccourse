b0d794a9a290b975c29b2de0206980c3f7aadd88
Vicixus99 <128025503+Vicixus99@users.noreply.github.com>
2025-04-22 14:51:09 +0530

    app.js

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-function initMap(){
-    var options = {
-        center: {lat: 38.3460 , lng:-0.4907 },
-        zoom: 10
+var myLatLng = { lat: 38.3460, lng: -0.4907 };
+var mapOptions = {
+    center: myLatLng,
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay = new google.maps.DirectionsRenderer();
+directionsDisplay.setMap(map);
+function calcRoute() {
+    var request = {
+        origin: document.getElementById("from").value,
+        destination: document.getElementById("to").value,
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.IMPERIAL
     }
-    map = new google.maps.Map(document.getElementById("map"),options)
-    google.maps.event.addListener(map, "click", (event) => {
-        addMarker({location:event.latLng});
-    })
-    let MarkerArray = [ 
-        {location:{lat: 37.9922, lng: -1.1307}, imageIcon: "https://img.icons8.com/nolan/2x/marker.png", content: `<h2>Murcia City</h2>`},
-        {location:{lat: 39.4699, lng: -0.3763}},
-        {location:{lat: 38.5411, lng: -0.1225},content: `<h2>Benidorm City</h2>`}
-    ]
-    for (let i = 0; i < MarkerArray.length; i++){
-        addMarker(MarkerArray[i]);
-    }
-    function addMarker(property){
-        const marker = new google.maps.Marker({
-            position:property.location,
-            map:map,
-        });
-        if(property.imageIcon){
-            marker.setIcon(property.imageIcon)
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            const output = document.querySelector('#output');
+            output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
+            directionsDisplay.setDirections(result);
+        } else {
+            directionsDisplay.setDirections({ routes: [] });
+            map.setCenter(myLatLng);
+            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
         }
-        if(property.content){
-            const detailWindow = new google.maps.InfoWindow({
-                content: property.content
-            });
-            marker.addListener("mouseover", () =>{
-                detailWindow.open(map, marker);
-            })
-        }
-    }
+    });
 }
+var options = {
+    types: ['(cities)']
+}
+var input1 = document.getElementById("from");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+var input2 = document.getElementById("to");
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
